92ad2ce79aea7be84623e3816d513779b369d586
Asma Samano <asmasamano@Asmas-MBP.home>
2023-05-13 17:22:43 +0100

    dups

--- codewars_practice/dup_count.py
+++ codewars_practice/dup_count.py
@@ -1,16 +1,14 @@
-# take a string and count the duplicate characters. 
-# Create an empty list, and add characters only if duplicate. 
-# then count the length of the list. 
+#count duplicate characters
 
 def duplicate_count(s):
-    #first create a dictionary
+    #first create a dictionary that stores each character and the number of it's dups
     chars = {}
     for char in s.lower():
         if char not in chars:
             chars[char] = 1
         else:
             chars[char] += 1
-    
+    #then only store the dups in a dict w/ a value of the characters repeated.
     duplicate = {}
 
     for char, count in chars.items():
@@ -19,7 +17,5 @@ def duplicate_count(s):
     return sum(duplicate.values())
 
     
-    
-
 print(duplicate_count("abcd"))
 
